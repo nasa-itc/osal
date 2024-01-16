fc9aa16ea4f1aaa6ddb20a31b69374bbc858c03a
Lucas, John P <John.P.Lucas@nasa.gov>
2024-01-16 17:08:00 -0500

    [nasa/nos3#210] Updated NOS_clock_nanosleep flags;

--- src/os/nos/src/os-impl-tasks.c
+++ src/os/nos/src/os-impl-tasks.c
@@ -721,16 +721,10 @@ int32 OS_TaskDelay_Impl(uint32 millisecond)
 {
     struct timespec sleep_end;
     int             status;
-    const int       MAX_SPEEDUP = 256;
-
-    //   Previous implementation using NOS instead of MAX_SPEEDUP
+    
     //NOS_clock_gettime(CLOCK_MONOTONIC, &sleep_end);
-    //sleep_end.tv_sec += millisecond / 1000;
-    //sleep_end.tv_nsec += 1000000 * (millisecond % 1000);
-
-    clock_gettime(CLOCK_MONOTONIC, &sleep_end);
-    sleep_end.tv_sec += (millisecond / 1000) / MAX_SPEEDUP;
-    sleep_end.tv_nsec += (1000000 * (millisecond % 1000)) / MAX_SPEEDUP;
+    sleep_end.tv_sec = millisecond / 1000;
+    sleep_end.tv_nsec = 1000000 * (millisecond % 1000);
 
     if (sleep_end.tv_nsec >= 1000000000)
     {
@@ -740,10 +734,7 @@ int32 OS_TaskDelay_Impl(uint32 millisecond)
 
     do
     {
-        //   Previous implementation using NOS instead of MAX_SPEEDUP
-        //status = NOS_clock_nanosleep(CLOCK_MONOTONIC, TIMER_ABSTIME, &sleep_end, NULL);
-
-        status = clock_nanosleep(CLOCK_MONOTONIC, TIMER_ABSTIME, &sleep_end, NULL);
+        status = NOS_clock_nanosleep(CLOCK_MONOTONIC, 0, &sleep_end, NULL);
     } while (status == EINTR);
 
     if (status != 0)
